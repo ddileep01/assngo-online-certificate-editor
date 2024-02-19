1acf6ae6ece815cf7bde92e24bb84cbda3d709d9
ddileep01 <dileep18052001@gmail.com>
2024-02-19 17:26:23 +0530

    feat: added mandatory to the input fields

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ function App() {
               className="w-full border border-gray-400 rounded-md p-2"
               value={formData.certificateName}
               onChange={handleInputChange}
+              required
             />
           </div>
           <div className="mb-4">
@@ -71,6 +72,7 @@ function App() {
               className="w-full border border-gray-400 rounded-md p-2"
               value={formData.fromDate}
               onChange={handleInputChange}
+              required
             />
           </div>
           <div className="mb-4">
@@ -84,6 +86,7 @@ function App() {
               className="w-full border border-gray-400 rounded-md p-2"
               value={formData.toDate}
               onChange={handleInputChange}
+              required
             />
           </div>
           <div className="mb-4">
@@ -97,6 +100,7 @@ function App() {
               className="w-full border border-gray-400 rounded-md p-2"
               value={formData.emailAddress}
               onChange={handleInputChange}
+              required
             />
           </div>
           <button className="bg-[#21403A] text-white px-4 py-2 flex items-center">
@@ -147,8 +151,11 @@ function App() {
               </span>
             </button>
             <p className="text-black text-center ml-2 mr-2 px-4 py-2">or</p>
-            <button className="bg-[#21403A] text-white px-4 py-2">
+            <button className="bg-[#21403A] text-white px-4 py-2 flex items-center">
               Send Email
+              <span className="ml-2">
+              <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512"><path fill="#ffffff" d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7 .3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2 .4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"/></svg>
+              </span>
             </button>
           </div>
         </div>
