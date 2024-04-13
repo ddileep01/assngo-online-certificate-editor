9a0bc3524f500f26ba0aa8eb105d0dc88015af80
Dileep Kumar Doddi <dileep18052001@gmail.com>
2024-04-13 12:02:44 +0530

    feat: added back CTA in generated certificate page

--- src/certificateview.js
+++ src/certificateview.js
@@ -3,7 +3,7 @@ import html2pdf from 'html2pdf.js';
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import './certificate.css';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 const CertificateView = ({ generatedCertificate }) => {
     const location = useLocation();
@@ -183,6 +183,9 @@ const CertificateView = ({ generatedCertificate }) => {
         >
           Send Email ✉
         </button>
+        <Link to="/" className="bg-green-950 hover:bg-green-900 text-white px-4 py-2 w-64 mt-2 mb-2">
+          Back to Generate Certificate
+        </Link>
       </div>
     </div>
   );
