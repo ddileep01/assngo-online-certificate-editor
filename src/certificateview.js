b830b825429556286b5f0b935c6fbba1ead7c4df
ddileep01 <dileep18052001@gmail.com>
2025-04-29 15:44:53 +0530

    fix: pdf styles bind

--- src/certificateview.js
+++ src/certificateview.js
@@ -19,7 +19,12 @@ const CertificateView = ({ generatedCertificate }) => {
       margin: 0,
       filename: 'ass-certificate.pdf',
       image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2, useCORS: true },
+      html2canvas: { 
+        scale: 2, 
+        useCORS: true,
+        letterRendering: true,
+        allowTaint: true
+      },
       jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }, // Change orientation to landscape
     };
   
@@ -140,54 +145,53 @@ const CertificateView = ({ generatedCertificate }) => {
           </div>
 
           <div className="contact">
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <div style={{ display: "flex", alignItems: "center", marginRight: "5px" }}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="12"
-                  width="12"
-                  viewBox="0 0 512 512"
-                  style={{ marginTop: "1px" }}
-                >
-                  <path
-                    fill="#000000"
-                    d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"
-                  />
-                </svg>
-              </div>
-              <p style={{ margin: "0 5px 0 0" }}>akhandasevasamsthan.ass@gmail.com</p>
-              <span style={{ margin: "0 10px" }}>|</span>
-              <div style={{ display: "flex", alignItems: "center", marginRight: "5px" }}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="12"
-                  width="12"
-                  viewBox="0 0 512 512"
-                  style={{ marginTop: "1px" }}
-                >
-                  <path
-                    fill="#000000"
-                    d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z"
-                  />
-                </svg>
-              </div>
-              <a href="https://www.akhandasevasamsthan.org/" style={{ textDecoration: "none", color: "inherit", margin: 0 }}>www.akhandasevasamsthan.org</a>
-            </div>
-            <p>
-              CERTIFICATE ID:{" "}
-              <u>
-                <a href="https://akhanda-seva-samsthan.vercel.app/validate">
-                  {props.certId}
-                </a>
-              </u>
-            </p>
+            <table style={{ width: "100%", margin: "0 auto", borderSpacing: "0", borderCollapse: "collapse" }}>
+              <tbody>
+                <tr>
+                  <td style={{ textAlign: "center" }}>
+                    <span style={{ display: "inline-block", marginRight: "5px", verticalAlign: "middle" }}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="12"
+                        width="12"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          fill="#000000"
+                          d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"
+                        />
+                      </svg>
+                    </span>
+                    <span style={{ verticalAlign: "middle" }}>akhandasevasamsthan.ass@gmail.com</span>
+                    <span style={{ margin: "0 10px", verticalAlign: "middle" }}>|</span>
+                    <span style={{ display: "inline-block", marginRight: "5px", verticalAlign: "middle" }}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="12"
+                        width="12"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          fill="#000000"
+                          d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z"
+                        />
+                      </svg>
+                    </span>
+                    <a href="https://www.akhandasevasamsthan.org/" style={{ textDecoration: "none", color: "inherit", verticalAlign: "middle" }}>www.akhandasevasamsthan.org</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ textAlign: "center", paddingTop: "5px" }}>
+                    CERTIFICATE ID:{" "}
+                    <u>
+                      <a href="https://akhanda-seva-samsthan.vercel.app/validate">
+                        {props.certId}
+                      </a>
+                    </u>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
